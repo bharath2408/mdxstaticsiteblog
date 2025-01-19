b0b7f89be1b7e6c738b83834b0d36ea87dedcaac
bharatha.kumar <bharatha.kumar@revalgo.ai>
2025-01-19 23:31:12 +0530

    add

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,29 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  /**
+   * Enable static exports for the App Router.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
   output: "export",
+
+  /**
+   * Set base path. This is usually the slug of your repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: "/mdxstaticsiteblog",
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
+   */
   images: {
     unoptimized: true,
   },
-  // Configure asset prefix
-  assetPrefix: ".",
-  // Force trailing slashes
-  trailingSlash: true,
-  // Disable page directory rewriting
-  skipTrailingSlashRedirect: true,
-  // Prevent index.html rewrites
-  skipMiddlewareUrlNormalize: true,
 };
 
 export default nextConfig;
