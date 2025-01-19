3a82728b6d933e10b3ff14f8b6b1503c16a4162c
bharatha.kumar <bharatha.kumar@revalgo.ai>
2025-01-19 22:52:43 +0530

    config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export", // Enable static exports
+  basePath: process.env.NODE_ENV === "production" ? "/mdxstaticsiteblog" : "", // Replace <repository-name> with your repo name
+  images: {
+    unoptimized: true, // Required for static export
+  },
+  // Disable trailing slashes
+  trailingSlash: false,
 };
 
 export default nextConfig;
